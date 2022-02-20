798e21b998360fbf07dd625aa4ef86a14e3772c1
lalit <lalit143.er@gmail.com>
2022-02-20 13:57:54 +0530

    optional parameter example private variable

--- main.dart
+++ main.dart
@@ -1,14 +1,14 @@
 main() {
-  var order1 = new Order(1, 'ref1', new DateTime.now());
+  var order1 = new Order(1, 'ref1');
   print(order1.getInfo());
 }
 
 class Order {
   int _id;
   String _reference;
-  DateTime _date;
+  DateTime? _date;
 
-  Order(this._id, this._reference, this._date) {
+  Order(this._id, this._reference, [this._date]) {
     // this.id = id;
     // this.reference = reference;
     // this.date = date;
