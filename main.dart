666e052400acf41dcf8b17152e86c362932e85cf
lalit <lalit143.er@gmail.com>
2022-02-20 14:33:44 +0530

    named constructor with no new keyword and named parameter and nullable variable

--- main.dart
+++ main.dart
@@ -1,25 +1,28 @@
 main() {
-  var order1 = new Order(1, 'ref1');
+  var order1 =  Order.highWay(1, 'ref1',codeName: "dartRocks");
   print(order1.getInfo());
 }
 
 class Order {
   int _id;
   String _reference;
-  DateTime? _date;
+  DateTime? date;
+  String? codeName;
 
-  Order(this._id, this._reference, [this._date]) {
+  Order(this._id, this._reference, {this.date}) {
     // this.id = id;
     // this.reference = reference;
     // this.date = date;
   }
+  Order.highWay(this._id, this._reference, {this.codeName}){} // named constructor
 
-  getInfo() {
+  String getInfo() {
     return '''Your order information:
         ----------------------------
         Id: $_id
         Reference: $_reference
-        Date: $_date
+        Date: $date
+        CodeName:$codeName
         ---------------------------''';
   }
 }
