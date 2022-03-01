be38395a5f948e5153397dd4e51427f33f3b21db
lalit <lalit143.er@gmail.com>
2022-03-01 21:07:09 +0530

    optional parameter is added at the end of all argumentes

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 main() {
-  var order1 =  Order.highWay(1, 'ref1',codeName: "dartRocks");
+  var order1 = Order.highWay(1, 'ref1', codeName: "dartRocks");
+  var order2 = Order.local(2, 'ref1', codeName: "dartRocks", name: "village road");
   print(order1.getInfo());
+  print(order2.getInfo());
 }
 
 class Order {
@@ -8,13 +10,18 @@ class Order {
   String _reference;
   DateTime? date;
   String? codeName;
+  String? name;
 
   Order(this._id, this._reference, {this.date}) {
     // this.id = id;
     // this.reference = reference;
     // this.date = date;
   }
-  Order.highWay(this._id, this._reference, {this.codeName}){} // named constructor
+
+  Order.highWay(this._id, this._reference,
+      {this.codeName}) {} // named constructor
+  Order.local(this._id, this._reference,
+      {this.codeName, this.name}) {} // named constructor
 
   String getInfo() {
     return '''Your order information:
